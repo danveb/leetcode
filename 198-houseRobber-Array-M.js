44c8edf8dbeebf36ec96f8bfa0a811442420c65b
Daniel Bae <danibae91@icloud.com>
2022-03-22 09:09:55 -0700

    update leetcode-75

---
+++ 198-houseRobber-Array-M.js
@@ -0,0 +1,52 @@
+/* House Robber (Leetcode #198) 
+
+Problem: Given an array "nums" representing money of each house, return max amount of money we can rob without robbing 2 adjacent houses. 
+
+nums = [1, 2, 3, 1] 
+output = 4; we rob house 1 (money = 1) and rob house 3 (money = 3). 3 + 1 = 4
+
+nums = [2, 7, 9, 3, 1]
+output = 12; rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1). 2 + 9 + 1 = 12 
+            
+idx     0   1   2   3
+num     5   50  7   10  => initialize a running sum in Array 
+        i               => [5]
+            i           => [5, 50] 
+                i       => [5, 50, 50] 
+                    i   => [5, 50, 50, 60]
+                    
+idx     0   1   2   3   4
+num     $8  $1  $3  $10 $7
+        i                   => max we can rob is $8 [8]
+            i               => max we can rob is $8 or $1, better rob $8 [8, 8] 
+                i           => we can rob $3 and add to $8 robbed from house 1 [8, 8, 11]
+                    i       => we can rob $10 and add to $8 robbed from house 1 [8, 8, 11, 18]
+                        i   => we can rob $7 and add to $11 robbed so far [8, 8, 11, 18, 18]
+
+Approach DYNAMIC PROGRAMMING -> Time: O(n) / Space: O(1) 
+- edge case: if length of nums array is === 0 we return 0
+- edge case: if length of nums array is < 2 we return MAX of nums array 
+- initialize dp array as size of nums array and filled with 0's 
+- initialize dp[0] as first element of nums array 
+- initialize dp[1] as second element of nums array
+- iterate over nums array (from 2nd index onwards until end) 
+- set dp[i] as MAX dp[i-1], nums[i] + dp[i-2]
+- return the last value which is the HIGHEST of dp array 
+
+*/ 
+
+function rob(nums) {
+    if(nums.length === 0) return 0
+    if(nums.length < 2) return Math.max(nums) 
+
+    // initialize dp array filled with 0's 
+    const dp = Array(nums.length).fill(0) 
+    dp[0] = nums[0] 
+    dp[1] = nums[1] 
+    // iterate from the 3rd element onwards 
+    for(let i = 2; i < nums.length; i++) {
+        dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]) 
+    }
+    // return the last value which is the highest of dp
+    return dp[dp.length - 1] 
+}
