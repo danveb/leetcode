000cf0e6c73caf16a20353cb938af8e0cd0f6e6b
Daniel Bae <danibae91@icloud.com>
2022-03-28 11:18:24 -0700

    update space complexity for algorithm1 and work on algorithm2

--- 125-validPalindrome-Str-E.js
+++ 125-validPalindrome-Str-E.js
@@ -24,7 +24,7 @@ str a   m   a   n   a   p   l   a   n   a   c   a   n   a   l   p   a   n   a   
                                            l/r                                          => c/c 
 
 Time: O(n) 
-Space: O(n) 
+Space: O(1) we don't need extra memory for data structures
 
 Notes: use regex and 2 pointers that will move inwards
 - convert any uppercase to lowercase 
@@ -35,6 +35,17 @@ Notes: use regex and 2 pointers that will move inwards
 - check: if str[left] !== str[right] return false
 - separately increase/decrease pointers 
 - return true in the end
+
+Notes 2: without regex and still use 2 pointers 
+- ,:~ space are all invalid characters that need to be removedremoved 
+
+Approach 2 
+- initialize left at 0 
+- initialize right at end of str 
+- keep looping while left <= right 
+- check: if str[left] is NOT equal to str[right] return false 
+- keep moving pointers inward LEFT++/RIGHT-- 
+- after checking everything return true in the end
 
 */ 
 
@@ -56,3 +67,37 @@ function isPalindrome(str) {
 
 console.log(isPalindrome('A man, a plan, a canal: Panama'))
 console.log(isPalindrome('race a car'))
+
+function isPalindrome2(str) {
+    let left = 0
+    let right = str.length - 1
+    while(left < right) {
+        // checks whether LEFT pointer is on VALID CHARACTER (left++ to keep checking)
+        while(!isValidCharacter(str.charAt(left))) {
+            left++ 
+        } 
+
+        // checks whether RIGHT pointer is on VALID CHARACTER (right-- to keep checking)
+        while(!isValidCharacter(str.charAt(right))) {
+            right--
+        }
+
+        // check if LOWERCASE characters are NOT EQUAL to return false 
+        if(str.charAt(left).toLowerCase() !== str.charAt(right).toLowerCase()) return false
+
+        // move pointers inward 
+        left++
+        right--
+    }   
+    return true 
+}
+
+// helper function for isPalindrome2
+function isValidCharacter(character) {
+    // initialize validCharacters as alpha-numeric 
+    let validCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789' 
+    return validCharacters.indexOf(character.toLowerCase()) > -1
+}
+
+console.log(isPalindrome2('A man, a plan, a canal: Panama'))
+console.log(isPalindrome2(''))
