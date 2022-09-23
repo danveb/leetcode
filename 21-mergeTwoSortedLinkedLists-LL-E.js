a7a2a5296566596caa4c3b260fa13cd04ce65db6
Daniel Bae <danibae91@icloud.com>
2022-09-22 17:46:35 -0700

    update code with comments

--- 21-mergeTwoSortedLinkedLists-LL-E.js
+++ 21-mergeTwoSortedLinkedLists-LL-E.js
@@ -1,90 +1,73 @@
 /* Merge Two Sorted Linked Lists (Leetcode #21)
 
-Problem: Given heads of two sorted linked lists, merge the two lists into 1 sorted list. 
-Return the head of merged linked list. 
+Problem: Given heads of two sorted linked lists (list1, list2), merge them to 1.
+Return the head of the merged linked list. 
 
-class Node {
-    constructor(value) {
-        this.value = value; 
-        this.next = null; 
-    };
-}; 
+list1 = [0, 3, 4]
+list2 = [1, 2, 6]
+output = [0, 1, 2, 3, 4, 6] 
 
-list1 = [1, 2, 4] 
-list2 = [1, 3, 4] 
-output = [1, 1, 2, 3, 4, 4] 
+list1   0   3   4   -> null
+list2   1   2   6   -> null
+--------------------------- 
+output  -1 -> 0   1   2   3   4   6 -> null 
 
-list1 = []
-list2 = []
-output = []
+Approach
+- since we don't have a node to start we'll want a dummy at -1
+- initialize head as the dummy so we can keep track of the head 
+- keep looping while list1 and list1 are !== null 
+- check which list goes first by comparing if value of list1 < value of list2
+- if true...  
+-- set head.next to point to this list1
+-- set list1 to list1.next 
+- else... 
+-- set head.next to point to list2
+-- set list2 to list2.next 
+- outside of while loop we want to check if list1 !== null
+-- if so, head.next = list1
+- outside of while loop we want to check if list2 !== null 
+-- if so, head.next = list2
+- RETURN DUMMY.NEXT since this is the start of head
 
-list1 = []
-list2 = [0] 
-output = [0] 
-
-list1   1   2   4   -> null
-list2   1   3   4   -> null
----------------------------
-output  -1 -> 1   1   2   3   4   4 -> null 
-
-list1   1   2   4
-list2   3   8   9   10  
----------------------------
-output  -1 -> 1   2   3   4   8   9   10 -> null 
-
-Notes 
-- we are given two linked lists and the head of each one
-- our goal is to merge the lists into 1 and return the head of linked list 
-- by default we can initialize newHead and make it a dummy with value of -1  
-- we can set newHead.next to p1 (if same value to p2) 
-- by having 2 extra pointers for p1/p2 referring to each linked list we are going to traverse
-- we can set a variable current set to each of the heads
-- we keep looping while current !== null 
-- check: if value of p1 <= p2 we'll go ahead and current.next to the p 
-- return the new head 
-
-Approach: 
-- initialize a dummyNode as -1 so it can be in front of the linked list (becoming new head) 
-- initialize head and set it as dummy 
-- keep looping while list1 && list2 have values 
-- check: if value in list1 is less than or equal to value in list2 
-- we will set dummy.next to point to the correct list (list1) 
-- else we will set dummy.next to point to the next list (list2) 
-- keep moving dummy to next node 
-- check: if list1 is not null we attach remaining elements of list1
-- else: attach elements of list2 
-- return head.next to return correct linked list (starting after dummy) 
-
-Time: O(n + m) where n + m are the lenghts of two linked lists
+Time: O(n + m) where n + m represents lengths of two linked lists 
 Space: O(1) we don't incur extra memory
+
+=== 
+
+class ListNode {
+	constructor(value) {
+		this.value = value; 
+		this.next = null; 
+	}
+}
 
 */ 
 
-// initialize class Node
-class Node {
-    constructor(value) {
-        this.value = value; 
-        this.next = null; 
-    }; 
-}; 
-
 function mergeTwoLists(list1, list2) {
-    // edge case: if both lists are empty we'll just return empty []
-    if(list1 === null && list2 === null) return []; 
-    let dummyHead = new Node(-1); 
-    let head = dummyHead; 
-    while(list1 !== null && list2 !== null) {
-        if(list1.value <= list2.value) {
-            dummyHead.next = list1; 
-            list1 = list1.next; 
-        } else {
-            dummyHead.next = list2; 
-            list2 = list2.next; 
-        }
-        // keep our dummyHead moving to the next
-        dummyHead = dummyHead.next; 
-    }
-    if(list1 !== null) dummyHead.next = list1;
-    if(list2 !== null) dummyHead.next = list2; 
-    return head.next; 
+	// edge case: if no linked lists we return null 
+	if(list1 === null && list2 === null) return null; 
+	let dummyHead = new ListNode(-1); 
+	let head = dummyHead; 
+	while(list1 !== null && list2 !== null) {
+		// check which list goes first 
+		// if both lists have same value either can go... 
+		// set head.next to list1 and list1 to list1.next
+		if(list1.value <= list2.value) {
+			head.next = list1; 
+			list1 = list1.next; 
+		} else {
+			// set head.next to list2 and list2 to list2.next
+			head.next = list2; 
+			list2 = list2.next; 
+		}
+		// keep looping head
+		head = head.next; 
+	}; 
+	// if either list is NOT NULL we want to continue with head.next
+	// already head -> head.next 
+	// now we need head.next -> list1 or list2
+	if(list1 !== null) head.next = list1; 
+	if(list2 !== null) head.next = list2; 
+	// return new HEAD OF LINKED LIST 
+	return dummyHead.next; 
 }
